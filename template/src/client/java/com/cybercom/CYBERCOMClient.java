c5697f12393b0bbfd46e80cc6e78f4104810f2eb
Unab-design <liamabidfontaine@gmail.com>
2025-11-05 14:55:46 +0100

    import template

---
+++ template/src/client/java/com/cybercom/CYBERCOMClient.java
@@ -0,0 +1,10 @@
+package com.cybercom;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CYBERCOMClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
